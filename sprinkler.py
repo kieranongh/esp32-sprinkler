cb966913d860973d0d7e65944251bdf0357e8326
kieranongh <KL>
2024-02-23 18:16:13 +1100

    Make a Sprinkler class and loop func

--- sprinkler.py
+++ sprinkler.py
@@ -3,6 +3,8 @@ import time
 
 ledPin = Pin(18, Pin.OUT)
 
-while True:
-  ledPin.value(not ledPin.value())
-  time.sleep(0.1)
+class Sprinkler:
+  def loop():
+    while True:
+      ledPin.value(not ledPin.value())
+      time.sleep(0.1)
