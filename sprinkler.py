ad60a3e96c1473283653c861788aa13f115cc3f7
kieranongh <KL>
2024-02-26 22:03:51 +1100

    Testing

--- sprinkler.py
+++ sprinkler.py
@@ -5,6 +5,7 @@ ledPin = Pin(2, Pin.OUT)
 
 class Sprinkler:
   def run(self):
+    print(f'Run')
     for i in range(50):
       ledPin.value(not ledPin.value())
       print(f'ledPin.value() => {ledPin.value()}', end='\r')
