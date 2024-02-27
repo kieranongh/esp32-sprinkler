5fea67f3ba0d65a85e5f5b89b9ee43d3fb27d553
kieranongh <KL>
2024-02-27 20:34:28 +1100

    Add on board button interrupt to trigger relay

--- sprinkler.py
+++ sprinkler.py
@@ -2,12 +2,24 @@ from machine import Pin
 import time
 
 ledPin = Pin(2, Pin.OUT)
+buttonPin = Pin(0, Pin.IN, pull=Pin.PULL_UP)
+relayPins = [
+  Pin(18, Pin.OUT)
+]
+
+def interrupter(pin):
+  relayPins[0].value(not relayPins[0].value)
 
 class Sprinkler:
   def run(self):
     print(f'Run')
+    buttonPin.irq(trigger=Pin.IRQ_FALLING, handler=interrupter)
     for i in range(50):
       ledPin.value(not ledPin.value())
       print(f'ledPin.value() => {ledPin.value()}', end='\r')
-      time.sleep(0.1)
+      if i%2 == 0:
+        time.sleep(0.1)
+      else:
+        time.sleep(0.4)
+
 
